fa342dde5a24ba8d9726f94685483a6459dd2a0b
ma2mori <tumamori6@gmail.com>
2024-11-10 18:27:45 +0900

    Firebaseの導入と初期設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:veloxorder/di/locator.dart';
@@ -10,9 +12,28 @@ import 'package:veloxorder/viewmodel/store/category/category_viewmodel.dart';
 import 'package:veloxorder/viewmodel/store/menu/menu_viewmodel.dart';
 import 'package:veloxorder/viewmodel/store/transaction/transaction_viewmodel.dart';
 import 'package:veloxorder/viewmodel/store/order/order_viewmodel.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Firestoreインスタンスの取得
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // テスト用のデータを書き込む
+  await firestore
+      .collection('test')
+      .doc('testDoc')
+      .set({'testField': 'testValue'});
+
+  // テスト用のドキュメントを取得
+  var testDoc = await firestore.collection('test').doc('testDoc').get();
+
+  print('Firestoreから取得したデータ: ${testDoc.data()}');
 
   await setupLocator();
 
