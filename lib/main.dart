6f0b4e2ec9f989d66e22bb55a1de7432264658f8
ma2mori <tumamori6@gmail.com>
2024-11-16 02:12:53 +0900

    不要なコード削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -20,9 +19,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  // Firestoreインスタンスの取得
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   await setupLocator();
 
