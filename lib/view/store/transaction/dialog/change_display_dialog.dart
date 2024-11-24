96a93db6d3a79badc9cc42091e615bf9c06e690e
ma2mori <tumamori6@gmail.com>
2024-11-25 08:06:07 +0900

    取引完了後QRコードを表示する

--- lib/view/store/transaction/dialog/change_display_dialog.dart
+++ lib/view/store/transaction/dialog/change_display_dialog.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_widget/barcode_widget.dart';
 
 class ChangeDisplayDialog extends StatelessWidget {
   final int changeAmount;
+  final String orderId;
 
-  const ChangeDisplayDialog({Key? key, required this.changeAmount})
-      : super(key: key);
+  const ChangeDisplayDialog({
+    Key? key,
+    required this.changeAmount,
+    required this.orderId,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String qrData =
+        'https://sample.app/order?orderId=$orderId'; // Todo ドメイnをenvから取得するように修正
+
     return AlertDialog(
-      title: Text('おつり'),
-      content: Text('おつりは ¥$changeAmount です。'),
+      title: Text('おつりと引換券'),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('おつりは ¥$changeAmount です。'),
+            SizedBox(height: 16),
+            Text('こちらのQRコードをお客様にお渡しください。'),
+            SizedBox(height: 16),
+            BarcodeWidget(
+              barcode: Barcode.qrCode(),
+              data: qrData,
+              width: 200,
+              height: 200,
+            ),
+          ],
+        ),
+      ),
       actions: [
         ElevatedButton(
           onPressed: () => Navigator.of(context).pop(),
